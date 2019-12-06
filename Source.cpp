c9bac359668fc194045b7c4b93b9a0fabe31c645
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-12-07 01:53:05 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+void sequenceReading()
+{
+
+}
+
+void sequenceSpaceProcessing()
+{
+
+}
+
+void sequenceSignsProcessing()
+{
+
+}
+
+void sequenceRegisterProcessing()
+{
+
+}
+
+void sequenceTask()
+{
+
+}
+
+int main()
+{
+	char* preparedString = new char[1024 * 1024];
+	delete[] preparedString;
+	system("pause");
+	return 0;
+}
