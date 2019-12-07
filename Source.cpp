2a6ca80183e9fd026ee1a6b677a733f21ab0ec74
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-12-07 03:03:13 +0300

    pointInWord() and sequenceReading() func.

--- Source.cpp
+++ Source.cpp
@@ -2,9 +2,36 @@
 
 using namespace std;
 
-void sequenceReading()
+bool pointInWord(char* word)
 {
+	for (short int i = 0; word[i] != '\0' && i != 256; i++)
+	{
+		if (word[i] == '.')
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
+void sequenceReading(char* preparedString, short int* indexOfLastItem)
+{
+	char* word = new char[256];
+	while (pointInWord(word) == false)
+	{
+		cin >> word;
+		for (short int i = 0; word[i] != '\0' && i != 256; i++)
+		{
+			preparedString[*indexOfLastItem] = word[i];
+			*indexOfLastItem += 1;
+		}
+		preparedString[*indexOfLastItem] = ' ';
+		*indexOfLastItem += 1;
+	}
+	*indexOfLastItem -= 1;
+	preparedString[*indexOfLastItem] = '\0';
+
+	delete[] word;
 }
 
 void sequenceSpaceProcessing()
@@ -30,7 +57,12 @@ void sequenceTask()
 int main()
 {
 	char* preparedString = new char[1024 * 1024];
-	delete[] preparedString;
+	short int* indexOfLastItem = new short int(0);
+	sequenceReading(preparedString, indexOfLastItem);
+	cout << preparedString;
 	system("pause");
+
+	delete[] preparedString;
+	delete indexOfLastItem;
 	return 0;
 }
