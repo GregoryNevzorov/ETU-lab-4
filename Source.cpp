ec2604bb8d318d70c1a2dfd20455c7a8349d1d34
GregoryNevzorov <gregoryofrivia@gmail.com>
2019-12-07 14:31:07 +0300

    update #1

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
 #define WORD_FROM_INPUT_STREAM_LAST_ITEM_INDEX 255
+#define PREPARED_STRING_LAST_ITEM_INDEX ((1024 * 1024) - 1)
 
 using namespace std;
 
@@ -14,6 +15,18 @@ bool pointInWord(char* wordFromInputStream)
 		}
 	}
 	return false;
+}
+
+bool excessSigns(char now, char buffer)
+{
+	if ((now == ',' and buffer == ',') or (now == ';' and buffer == ';') or (now == ':' and buffer == ':'))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 void sequenceReading(char* preparedString, short int* indexOfLastItem)
@@ -36,14 +49,59 @@ void sequenceReading(char* preparedString, short int* indexOfLastItem)
 	delete[] wordFromInputStream;
 }
 
-void sequenceSpaceProcessing()
+void sequenceExcessSignsProcessing(char* preparedString)
 {
-
+	char* buffer = new char(' ');
+	for (short int i = 0; preparedString[i] != '\0' && i != PREPARED_STRING_LAST_ITEM_INDEX; i++)
+	{
+		if (preparedString[i - 1] != ' ' and i != 0)
+		{
+			*buffer = preparedString[i - 1];
+		}
+		if (excessSigns(preparedString[i], *buffer) == true)
+		{
+			preparedString[i] = ' ';
+		}
+	}
+	delete buffer;
 }
 
-void sequenceSignsProcessing()
+void sequenceSignsTruePositionsProcessing(char* preparedString, short int* indexOfLastItem)
 {
-
+	short int* counter = new short int(1);
+	while (*counter != 0)
+	{
+		*counter = 0;
+		for (short int i = 0; preparedString[i] != '\0' && i != PREPARED_STRING_LAST_ITEM_INDEX; i++)
+		{
+			if (preparedString[i] == '.' and preparedString[i - 1] == ' ')
+			{
+				preparedString[i - 1] = '.';
+				preparedString[i] = '\0';
+				*indexOfLastItem -= 1;
+				break;
+			}
+			if (preparedString[i] == ',' and preparedString[i - 1] == ' ')
+			{
+				preparedString[i - 1] = ',';
+				preparedString[i] = ' ';
+				*counter += 1;
+			}
+			else if (preparedString[i] == ';' and preparedString[i - 1] == ' ')
+			{
+				preparedString[i - 1] = ';';
+				preparedString[i] = ' ';
+				*counter += 1;
+			}
+			else if (preparedString[i] == ':' and preparedString[i - 1] == ' ')
+			{
+				preparedString[i - 1] = ':';
+				preparedString[i] = ' ';
+				*counter += 1;
+			}
+		}
+	}
+	delete counter;
 }
 
 void sequenceRegisterProcessing()
@@ -61,7 +119,14 @@ int main()
 	char* preparedString = new char[1024 * 1024];
 	short int* indexOfLastItem = new short int(0);
 	sequenceReading(preparedString, indexOfLastItem);
-	cout << preparedString;
+	cout << preparedString << "\n";
+	cout << *indexOfLastItem << "\n";
+	sequenceExcessSignsProcessing(preparedString);
+	cout << preparedString << "\n";
+	cout << *indexOfLastItem << "\n";
+	sequenceSignsTruePositionsProcessing(preparedString, indexOfLastItem);
+	cout << preparedString << "\n";
+	cout << *indexOfLastItem << "\n";
 	system("pause");
 
 	delete[] preparedString;
